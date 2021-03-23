15bbd39fb5924dd508eff3de2805e78624c24c0c
henrique <henriqueeeidt011@gmail.com>
2021-03-22 23:00:57 -0300

    commit

--- autocorrelação/Autocorrelação.py
+++ autocorrelação/Autocorrelação.py
@@ -1,3 +1,4 @@
+from numpy.core.defchararray import index
 import pandas_datareader as wb
 import pandas as pd
 import statsmodels.api as sm
@@ -5,7 +6,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 from matplotlib import style
-
 
 
 #Pega automaticamente todos os dados das ações e índices do yahoo finance para as datas em questão
@@ -24,24 +24,16 @@ df.insert(loc=11, column = 'lnITSA3', value =  np.log(df['ITSA3.SA']/df['ITSA3.S
 df.insert(loc=13, column = 'lnDIRR3', value =  np.log(df['DIRR3.SA']/df['DIRR3.SA'].shift()))
 df.insert(loc=13, column = 'lnEVEN3', value =  np.log(df['EVEN3.SA']/df['EVEN3.SA'].shift()))
 
-
-#Coloca o df em um csv, é necessário pois os log retornos geram valores 'NAN' na primeira linha, o que quebra o código a seguir
-df.to_csv('trabalho.csv')
-
-#Lê o arquivo csv pulando a primeira linha que contêm os valores 'NAN'
-df = pd.read_csv('trabalho.csv',skiprows=[1])
+#Elimina a primeira linha, pois ela contêm valores NAN
+df = df.iloc[1:]
 
 
 ibov = sm.add_constant(df['lnIBOV'])
 print(df)
 
-#Define o modelo endog=y, exog=x
+#ITSA3 apenas para fins de teste
 mod = sm.OLS(endog=df['lnITSA3'],exog= ibov)
-
-#Roda o modelo
 res = mod.fit()
-
-#Printa os resultados
 print(res.summary())
 
 
@@ -74,44 +66,21 @@ mod_even3 = sm.OLS(endog=df['lnEVEN3'],exog= ibov)
 res_even3 = mod_even3.fit()
 print(res_even3.summary())
 
-#teqwe = res_cyre3.params
 
-
-lista = res_cyre3,res_dirr3,res_even3,res_eztc3,res_jhsf3,res_tris3
-teste = []
-for i in lista:
-    teste.append(i.params.iloc[0])
-print(teste)
-
-
-
-
-
-
-#Gera um df apenas de log retornos
-returns = ['lnCYRE3','lnEZTC3','lnJHSF3','lnTRIS3','lnITSA3','lnDIRR3','lnEVEN3']
-
-df_resid = pd.DataFrame(df['Date'])
-for i in returns:
-    df_resid[i] = df[i]
-df_resid.set_index('Date',inplace=True)
-
-#Converte as datas do formato string(padrão do pandas) para datetime. Sem esse procedimento o gráfico fica problemático
-dates = df_resid.index.tolist()
+#Cria uma lista com as datas do DataFrame convertidas para o formato matplotlib
+dates = df.index.tolist()
 dates = [pd.to_datetime(d) for d in dates]
 
-#Estilos
-style.use('seaborn')
 
 #O restante é apenas configuração do gráfico
+style.use('seaborn')
 fig,axs = plt.subplots()
 
-axs.scatter(x=dates,y=df_resid['lnEVEN3'],color='cornflowerblue',s=20)
+axs.scatter(x=dates,y=df['lnEVEN3'],color='cornflowerblue',s=20)
 axs.axhline(0,color='black')
 axs.set_xlabel('Tempo')
 axs.set_ylabel('Resíduos')
 axs.set_title('EVEN3')
-
 
 fig.suptitle("Resíduos em função do tempo", size=20)
 fig.autofmt_xdate()
